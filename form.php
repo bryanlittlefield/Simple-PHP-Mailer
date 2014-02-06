db5179df92497053a08ceee28c257f481cf12944
Aproducktion <Aproducktion@gmail.com>
2014-02-06 11:26:58 -0800

    Create form.php

---
+++ form.php
@@ -0,0 +1,21 @@
+<form class="home-form" method="POST" action="form-mailer.php">
+	<table border="0" width="340">
+		<tr>
+			<td><p>Name:</p></td>
+			<td><input name="name" type="text"></td>
+		</tr>
+		<tr>
+			<td><p>Email:</p></td>
+			<td><input name="email" type="text"></td>
+		</tr>
+		<tr>
+			<td><p>Phone:</p></td>
+			<td><input name="phone" type="text"></td>
+		</tr>
+		<tr>
+			<td><p>Comments:</p></td>
+			<td><input name="comments" type="text"></td>
+		</tr>
+	</table>
+	<button>Submit</button>
+</form>
